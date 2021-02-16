8230d063294fcab83fbd6d8a2c8194632e8883d5
Ilya Sinichkin <66882691+natatory@users.noreply.github.com>
2021-02-16 18:01:35 +0300

    Update App.xaml.cs
    master 6

--- AccountingSystemUI/App.xaml.cs
+++ AccountingSystemUI/App.xaml.cs
@@ -20,7 +20,7 @@ namespace AccountingSystemUI
 
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
-            ///test git master 5
+            ///test git master 6
             App.Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
             var ninjectKernel = new StandardKernel();
             ninjectKernel.Load(new CommonModule());
