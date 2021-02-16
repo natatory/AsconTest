942324638c4cf21c4e833a10a0e34367e2003478
natatory <natatory@icloud.com>
2021-02-16 18:04:11 +0300

    commit stashes 5

--- AccountingSystemUI/App.xaml.cs
+++ AccountingSystemUI/App.xaml.cs
@@ -15,7 +15,7 @@ namespace AccountingSystemUI
     /// </summary>
     public partial class App : System.Windows.Application
     {
-        //test git stashes 4
+        //test git stashes 5
         StartWindow startWindow;
 
         private void App_OnStartup(object sender, StartupEventArgs e)
